dcb89a94c68cc143e6716edecf8ab810048e7391
Benjamin Oakes <hello@benjaminoakes.com>
2014-10-04 11:20:13 -0500

    Force SSL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  force_ssl if: :force_ssl?
   http_basic_authenticate_with name: ENV['DOCCASTER_AUTH_NAME'], password: ENV['DOCCASTER_AUTH_PASSWORD']
+
+  private
+
+  def force_ssl?
+    Rails.env.production? && !request.user_agent.match('TinyTinyNAS')
+  end
 end
